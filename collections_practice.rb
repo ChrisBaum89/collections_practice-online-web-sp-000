3cb646d1f9db9f25d1aaa54b0fd03f80f4b1224d
Chris Baum <christopher.baum@valpo.edu>
2021-01-15 00:30:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,6 +7,10 @@ def sort_array_desc(array)
   array.sort.reverse
 end
 
+def sort_array_char_count(array)
+  array.sort_by{|element| length}
+end
+
 def swap_elements(array)
   array_temp_1 = array[1]
   array_temp_2 = array [2]
